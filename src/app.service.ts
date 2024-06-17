c28d01074f08105712e74d4de5bbd4e9ca0f2b32
Dezarnaud Julian <julian.dezarnaud@etu.unice.fr>
2024-06-17 18:00:44 +0200

    enr

--- src/app.service.ts
+++ src/app.service.ts
@@ -10,10 +10,14 @@ export class AppService {
     return 'Hello World!';
   }
 
+
+
   async saveFile(file: Express.Multer.File): Promise<string> {
     try {
+      var moment = require('moment');
+      moment().format('yyyy-mm-dd:hh:mm:ss');
       const uploadPath = path.join(__dirname, '..', 'capturesImages');
-      const filePath = path.join(uploadPath, 'photo.jpg');
+      const filePath = path.join(uploadPath, moment()+'.jpg');
       
       if (!fs.existsSync(uploadPath)) {
         fs.mkdirSync(uploadPath, { recursive: true });

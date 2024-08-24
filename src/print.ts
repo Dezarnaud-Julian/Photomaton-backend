884eed5f7330a2238b4b264a837b0353ea90d396
Dezarnaud Julian <julian.dezarnaud@etu.unice.fr>
2024-08-24 20:25:09 +0200

    polaroid

--- src/print.ts
+++ src/print.ts
@@ -30,38 +30,38 @@ export class PrintService {
         const pdfDoc = await PDFDocument.create();
         const jpgImage = await pdfDoc.embedJpg(jpgImageBytes);
 
-        let page = pdfDoc.addPage([jpgImage.width, jpgImage.height]);
-        let imgWidth = jpgImage.width;
-        let imgHeight = jpgImage.height;
+        if(template === 'POLAROID'){
+            // Dimensions du format 4x6 pouces en points (1 pouce = 72 points)
+            const pdfWidth = 720;
+            const pdfHeight = 1080; 
 
-        if (template === 'POLAROID') {
-            const pdfWidth = 288;
-            const pdfHeight = 432;
-
-            imgWidth = jpgImage.width / 2;
-            imgHeight = jpgImage.height / 2;
+            const imgWidth = jpgImage.width/1.6;
+            const imgHeight = jpgImage.height/1.6;
 
             const x = (pdfWidth - imgWidth) / 2;
             const y = (pdfHeight - imgHeight) / 1.06;
 
-            page = pdfDoc.addPage([pdfWidth, pdfHeight]);
+            const page = pdfDoc.addPage([pdfWidth, pdfHeight]);
             page.drawImage(jpgImage, {
                 x: x,
                 y: y,
                 width: imgWidth,
                 height: imgHeight,
             });
+
+            const pdfBytes = await pdfDoc.save();
+            await fs.writeFile(pdfPath, pdfBytes);
+        }else{
+            const page = pdfDoc.addPage([jpgImage.width, jpgImage.height]);
+            page.drawImage(jpgImage, {
+                x: 0,
+                y: 0,
+                width: jpgImage.width,
+                height: jpgImage.height,
+            });
+            const pdfBytes = await pdfDoc.save();
+            await fs.writeFile(pdfPath, pdfBytes);
         }
-
-        page.drawImage(jpgImage, {
-            x: 0,
-            y: 0,
-            width: imgWidth,
-            height: imgHeight,
-        });
-
-        const pdfBytes = await pdfDoc.save();
-        await fs.writeFile(pdfPath, pdfBytes);
     }
 
     async print(filePath: string, copiesRequested: number, template: string) {
@@ -112,6 +112,11 @@ export class PrintService {
                 await Promise.all(printPromises);
             } else {
                 // on linux
+                // lpoptions -l
+                // PageSize/Media Size: w288h288 w288h288-div2 *w288h216 w288h288_w288h144 w288h432 w288h432-div2 w288h432-div3 w288h576 w288h432_w288h144 w288h432-div2_w288h144 w288h576-div2 w288h576-div4 w324h216 w324h288 w324h324 w324h432 w324h432-div2 w324h486 w324h432-div3 w324h576 w324h576-div2 w324h576-div4 w324h432_w324h144 w324h432-div2_w324h144
+                // w288h216 = 4x3
+                // w288h288 = 4x4
+                // w288h432 = 4x6
                 const options = [`-n ${copiesToPrint}`, `-o PageSize=w288h432`];
                 console.log("Linux driver with options", options);
                 await getPrinters().then(console.log);

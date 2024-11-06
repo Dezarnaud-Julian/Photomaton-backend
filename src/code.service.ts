29a3e8bd78e49146c310fdfa48d7f5bd0c4fb7ae
Julian Dezarnaud <julian.dezarnaud@etu.unice.fr>
2024-11-06 20:34:53 +0100

    backend update

--- src/code.service.ts
+++ src/code.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
-import * as fs from 'fs';
-import * as path from 'path';
+import { readFile, writeFile } from 'fs/promises';
+import path from 'path';
 
 @Injectable()
 export class CodeService {
@@ -10,30 +10,32 @@ export class CodeService {
     this.loadCodes();
   }
 
-  // Charge les codes depuis `codes.txt` et initialise chaque code avec `true` (non utilisé).
-  private loadCodes() {
+  // Charge les codes depuis `codes.json` et initialise chaque code avec `false` (non utilisé).
+  private async loadCodes() {
     try {
-      const filePath = path.join(__dirname, '../src/codes.txt');
-      const data = fs.readFileSync(filePath, 'utf8'); // Utilisation de readFileSync pour une lecture synchrone
-      const codes = data.split('\n').map(line => line.trim()).filter(line => line);
-      codes.forEach(code => {
-        this.codeDictionary[code] = true;
-      });
+      const codesData = await readFile('codes.json');
+      this.codeDictionary = JSON.parse(codesData.toString()) as { [key: string]: boolean };
       console.log('Codes loaded successfully:', this.codeDictionary);
     } catch (error) {
       console.error('Error loading codes:', error);
       throw new BadRequestException('Failed to load codes');
     }
-}
+  }
+
+  async saveCodesInFile(codes: { [key: string]: boolean }) {
+    await writeFile('codes.json', JSON.stringify(codes), 'utf8');
+  };
+  
 
   // Vérifie le code scanné
   checkCode(code: string): string {
     if (!(code in this.codeDictionary)) {
       return 'Code invalide';
-    } else if (!this.codeDictionary[code]) {
+    } else if (this.codeDictionary[code]) {
       return 'Code déjà utilisé';
     } else {
-      this.codeDictionary[code] = false; // Marque le code comme utilisé
+      this.codeDictionary[code] = true; // Marque le code comme utilisé
+      this.saveCodesInFile(this.codeDictionary);
       return 'IMPRESSION';
     }
   }
